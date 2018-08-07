67478c9f8a1dd580e32712f4b76aa0ed7ef2a62b
tanelam <tania.e.aparicio@gmail.com>
2018-08-07 17:38:20 -0400

    DOM selectors for multiple elements

--- Examining-the-DOM/app.js
+++ Examining-the-DOM/app.js
@@ -1,43 +1,79 @@
-let val;
+// let val;
+//
+// val =  document;
+// val = document.all;
+// val = document.all[0];
+// val = document.all[2];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
+//
+// val =  document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
+//
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].id;
+// val = document.links[0].className;
+// val = document.links[0].classList;
+// val = document.links[0].classList[0];
+//
+// val = document.images;
+//
+// val = document.scripts;
+// val = document.scripts[2].getAttribute('src');
+//
+// let scripts = document.scripts;
+//
+// let scriptsArr = Array.from(scripts);
+//
+// scriptsArr.forEach(function(script){
+//   console.log(script)
+// })
+//
+//
+// console.log(val);
 
-val =  document;
-val = document.all;
-val = document.all[0];
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
-
-val =  document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
-
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList;
-val = document.links[0].classList[0];
-
-val = document.images;
-
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
-
-let scripts = document.scripts;
-
-let scriptsArr = Array.from(scripts);
-
-scriptsArr.forEach(function(script){
-  console.log(script)
-})
+//document.getElementById("task-title")
 
 
-console.log(val);
+console.log(document.getElementById("task-title"))
+
+//Get things from elememt
+console.log(document.getElementById("task-title").id)
+console.log(document.getElementById("task-title").className)
+
+const taskTitle = document.getElementById("task-title");
+
+//Change styling
+taskTitle.style.background = "#333"
+taskTitle.style.color = "#fff"
+// taskTitle.style.display = "none"
+
+//Change content
+taskTitle.textContent = " Task list";
+taskTitle.innerText = "My Tasks";
+taskTitle.innerHTML = "<span style='color: red'>Task List</span>";
+
+//docuemnt.querySelector()
+
+console.log(document.querySelector('#task-title'))
+console.log(document.querySelector('.card-title'))
+console.log(document.querySelector('h5'))
+
+document.querySelector('li').style.color = "red"
+document.querySelector('ul li').style.color = "blue"
+
+document.querySelector('li:last-child').style.color = "red"
+document.querySelector('li:nth-child(3)').style.color = "yellow"
+document.querySelector('li:nth-child(4)').textContent = "Hello World"
+document.querySelector('li:nth-child(odd)').style.background = "#ccc"
+document.querySelector('li:nth-child(even)').style.background = "#f4f4f4"
