66d90a5243083046f2f81ccf6ecfad2064d854b3
tanelam <tania.e.aparicio@gmail.com>
2018-08-07 20:53:37 -0400

    DOM selector for multiple elements

--- Examining-the-DOM/app.js
+++ Examining-the-DOM/app.js
@@ -44,36 +44,88 @@
 
 //document.getElementById("task-title")
 
+//
+// console.log(document.getElementById("task-title"))
+//
+// //Get things from elememt
+// console.log(document.getElementById("task-title").id)
+// console.log(document.getElementById("task-title").className)
+//
+// const taskTitle = document.getElementById("task-title");
+//
+// //Change styling
+// taskTitle.style.background = "#333"
+// taskTitle.style.color = "#fff"
+// // taskTitle.style.display = "none"
+//
+// //Change content
+// taskTitle.textContent = " Task list";
+// taskTitle.innerText = "My Tasks";
+// taskTitle.innerHTML = "<span style='color: red'>Task List</span>";
+//
+// //docuemnt.querySelector()
+//
+// console.log(document.querySelector('#task-title'))
+// console.log(document.querySelector('.card-title'))
+// console.log(document.querySelector('h5'))
+//
+// document.querySelector('li').style.color = "red"
+// document.querySelector('ul li').style.color = "blue"
+//
+// document.querySelector('li:last-child').style.color = "red"
+// document.querySelector('li:nth-child(3)').style.color = "yellow"
+// document.querySelector('li:nth-child(4)').textContent = "Hello World"
+// document.querySelector('li:nth-child(odd)').style.background = "#ccc"
+// document.querySelector('li:nth-child(even)').style.background = "#f4f4f4"
 
-console.log(document.getElementById("task-title"))
+//document.getElementsByClassName
 
-//Get things from elememt
-console.log(document.getElementById("task-title").id)
-console.log(document.getElementById("task-title").className)
+// const items = document.getElementsByClassName("collection-item")
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = "red";
+// items[3].textContent = "Hello!";
+//
+// const listItems =  document.querySelector("ul").getElementsByClassName("collection-item");
+//
+// console.log(listItems)
 
-const taskTitle = document.getElementById("task-title");
+// //document.getElementByTagName
+// let lis = document.getElementsByTagName("li")
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = "red";
+// lis[3].textContent = "Hello!";
 
-//Change styling
-taskTitle.style.background = "#333"
-taskTitle.style.color = "#fff"
-// taskTitle.style.display = "none"
+//Convert HTML collection into Array
 
-//Change content
-taskTitle.textContent = " Task list";
-taskTitle.innerText = "My Tasks";
-taskTitle.innerHTML = "<span style='color: red'>Task List</span>";
+// lis = Array.from(lis);
+//
+// lis.reverse();
+//
+// lis.forEach(function(li, index){
+//   console.log(li.className)
+//   li.textContent = `${index}: Hello!`
+// })
+//
+// console.log(lis)
 
-//docuemnt.querySelector()
+//document.querySelectorAll
 
-console.log(document.querySelector('#task-title'))
-console.log(document.querySelector('.card-title'))
-console.log(document.querySelector('h5'))
+const items = document.querySelectorAll('ul.collection li.collection-item');
 
-document.querySelector('li').style.color = "red"
-document.querySelector('ul li').style.color = "blue"
+items.forEach(function(item, index){
+  item.textContent = `${index}: Hello!`
+});
 
-document.querySelector('li:last-child').style.color = "red"
-document.querySelector('li:nth-child(3)').style.color = "yellow"
-document.querySelector('li:nth-child(4)').textContent = "Hello World"
-document.querySelector('li:nth-child(odd)').style.background = "#ccc"
-document.querySelector('li:nth-child(even)').style.background = "#f4f4f4"
+const liOdd =  document.querySelectorAll('li:nth-child(odd)');
+const liEven =  document.querySelectorAll('li:nth-child(even)');
+
+liOdd.forEach(function(li, index){
+  li.style.background = "#ccc"
+})
+for(let i = 0; i < liEven.length; i++){
+  liEven[i].style.background = "#f4f4f4"
+}
+
+console.log(items);
