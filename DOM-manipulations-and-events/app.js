838ddcce29841bd21862d8e57af425fe0cb5cb88
tanelam <tania.e.aparicio@gmail.com>
2018-08-11 16:19:31 -0400

    event bubbling and delegation

--- DOM-manipulations-and-events/app.js
+++ DOM-manipulations-and-events/app.js
@@ -388,3 +388,47 @@
 //   //
 //   // e.preventDefault();
 // }
+
+//event bubbling
+// document.querySelector('.card-title').addEventListener('click',
+//   function(){
+//     console.log('card-title')
+//   }
+// )
+//
+// document.querySelector('.card-content').addEventListener('click',
+//   function(){
+//     console.log('card-content')
+//   }
+// )
+//
+// document.querySelector('.card').addEventListener('click',
+//   function(){
+//     console.log('card')
+//   }
+// )
+//
+// document.querySelector('.col').addEventListener('click',
+//   function(){
+//     console.log('col')
+//   }
+// )
+//
+// const delItem = document.querySelector('.delete-item');
+//
+// delItem.addEventListener('click', deleteItem);
+
+// document.body.addEventListener('click', deleteItem)
+//
+// function deleteItem(e){
+//
+//   // if(e.target.parentElement.className === 'delete-item secondary-content'){
+//   //   console.log(e.target)
+//   // }
+//
+//
+//   if(e.target.parentElement.classList.contains('delete-item')){
+//     console.log(e.target)
+//     e.target.parentElement.parentElement.remove();
+//   }
+// }
