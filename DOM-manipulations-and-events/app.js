ff4dc02fa5b0f2b13f27405843da9c6a629e45fb
tanelam <tania.e.aparicio@gmail.com>
2018-08-11 15:44:55 -0400

    key board and input events

--- DOM-manipulations-and-events/app.js
+++ DOM-manipulations-and-events/app.js
@@ -340,19 +340,51 @@
 //   document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`
 // }
 
-const form =  document.querySelector('form');
-const taskInput = document.getElementById('task')
-
-//clear input
-taskInput.value = "";
+// const form =  document.querySelector('form');
+// const taskInput = document.getElementById('task')
+// const heading = document.querySelector('h5')
+//
+// //clear input
+// taskInput.value = "";
 
 // form.addEventListener('submit', runEvent)
 
-function runEvent(e){
-  console.log(`EVENT Type: ${e.type}`);
+// //keydown
+// taskInput.addEventListener("keydown", runEvent)
+//
+// //keyup
+// taskInput.addEventListener("keyup", runEvent)
 
-  //get input value
-  // console.log(taskInput.value)
-  //
-  // e.preventDefault();
-}
+// //keypress
+// taskInput.addEventListener("keypress", runEvent)
+
+// //focus
+// taskInput.addEventListener("focus", runEvent)
+//
+// //blur
+// taskInput.addEventListener("blur", runEvent)
+
+// //cut
+// taskInput.addEventListener("cut", runEvent)
+//
+// //paste
+// taskInput.addEventListener("paste", runEvent)
+
+// //input
+// taskInput.addEventListener("input", runEvent)
+
+// //input
+// taskInput.addEventListener("input", runEvent)
+//
+//
+// function runEvent(e){
+//   console.log(`EVENT Type: ${e.type}`);
+//   console.log(e.target.value)
+//
+//   // heading.innerText = e.target.value
+//
+//   //get input value
+//   // console.log(taskInput.value)
+//   //
+//   // e.preventDefault();
+// }
