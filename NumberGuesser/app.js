35742c9ec2f097c204ab23a3b80f17ea8bf0d7b4
tanelam <tania.e.aparicio@gmail.com>
2019-01-03 14:12:44 -0500

    Added logic to game

--- NumberGuesser/app.js
+++ NumberGuesser/app.js
@@ -27,20 +27,39 @@ guessBtn.addEventListener("click", function(){
     setMessage(`Please enter a number between ${min} and ${max}`, "red")
   }
 
-  if(guess === winningNum ){
+  if(guess === winningNum){
     //Disabled Input
-    guessInput.disabled = true;
-    //Border Color
-    guessInput.style.borderColor = "green";
+    gameOver(true, `${winningNum} is correct, YOU WIN!`)
     //Let user know they won
-    setMessage(`${winningNum} is correct, YOU WIN!`, "green")
   }else{
+
     guessesLeft -= 1;
-    if (guessesLeft === 0)
+
+    if (guessesLeft === 0){
       //Game over lost
+      gameOver(false, `Game Over, you lost! The winning number was ${winningNum}`)
+
+      guessBtn.disabled = true;
+    }else {
+      //Game continues if answer wrong
+      gameOver(false, `${guess} is not correct, ${guessesLeft} guesses left`)
+
+      guessInput.value = " ";
     }
   }
 })
+
+//Game over function
+
+function gameOver(won, message){
+  let color;
+  won === true ? color = "green" : color = "red"
+
+  guessInput.disabled = won;
+  guessInput.style.borderColor = color;
+  setMessage(message, color)
+}
+
 
 function setMessage(msg, color){
   message.style.color = color
