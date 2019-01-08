d656154f59b78a8d114c580dc275d344791fc2ae
tanelam <tania.e.aparicio@gmail.com>
2019-01-08 17:46:13 -0500

    added put and delete methods

--- Custom-HTTP-Library/app.js
+++ Custom-HTTP-Library/app.js
@@ -27,11 +27,31 @@ const data = {
 };
 
 //post posts
-http.post("https://jsonplaceholder.typicode.com/posts", data,
-  function(err, post){
+// http.post("https://jsonplaceholder.typicode.com/posts", data,
+//   function(err, post){
+//     if (err) {
+//       console.log(err)
+//     } else {
+//       console.log(post)
+//     }
+// })
+
+//update post
+// http.put("https://jsonplaceholder.typicode.com/posts/1", data,
+//   function(err, post){
+//     if (err) {
+//       console.log(err)
+//     } else {
+//       console.log(post)
+//     }
+// })
+
+//delete post
+http.delete("https://jsonplaceholder.typicode.com/posts/1",
+  function(err, response){
     if (err) {
       console.log(err)
     } else {
-      console.log(post)
+      console.log(response)
     }
 })
