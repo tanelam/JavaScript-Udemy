b145490e1187423964de7dd1b80b979f0b034e91
tanelam <tania.e.aparicio@gmail.com>
2019-01-22 17:44:00 -0500

    display the resutls from the api in ui

--- Weatherjs/app.js
+++ Weatherjs/app.js
@@ -1,6 +1,9 @@
 //init weather class
 const weather = new Weather("Boston", "MA");
 
+//init new ui
+const ui = new UI();
+
 // weather.changeLocation("Miami", "FL")
 
 //get weather on dom load
@@ -8,6 +11,8 @@ document.addEventListener("DOMContentLoaded", getWeather)
 
 function getWeather(){
   weather.getWeather()
-    .then(results => console.log(results))
-    .catch(err => console.log(err ))
+    .then(results => {
+      ui.paint(results);
+    })
+    .catch(err => console.log(err))
 }
