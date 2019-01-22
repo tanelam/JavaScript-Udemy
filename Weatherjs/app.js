42248d97d801b2585c613244c4e4a97339374d05
tanelam <tania.e.aparicio@gmail.com>
2019-01-22 18:09:03 -0500

    added function to change location

--- Weatherjs/app.js
+++ Weatherjs/app.js
@@ -7,7 +7,21 @@ const ui = new UI();
 // weather.changeLocation("Miami", "FL")
 
 //get weather on dom load
-document.addEventListener("DOMContentLoaded", getWeather)
+document.addEventListener("DOMContentLoaded", getWeather);
+
+//change location event
+document.getElementById("w-change-btn").addEventListener("click", (event) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  weather.changeLocation(city, state);
+
+  //get and display weather
+  getWeather();
+
+  //close the modal
+  $('#locModal').modal('hide')
+})
 
 function getWeather(){
   weather.getWeather()
